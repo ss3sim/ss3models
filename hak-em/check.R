0073e143f6642eef3f0b76fa485668b52732bbd1
Sean Anderson <github@seananderson.ca>
2015-02-05 01:20:56 -0500

    Enhance the EM checking R script

--- hak-em/check.R
+++ hak-em/check.R
@@ -1,5 +1,13 @@
-file.copy("../hak-om/hakeOM.dat", "hakeEM.dat")
-library("r4ss")
-system("ss3_24o_safe")
-o <- SS_output(dir = ".")
-SS_plots(o, png = TRUE)
+species <- "hake"
+
+original_files <- list.files()
+file.copy(paste0("../", species, "-om/", species, "OM.dat"),
+  paste0(species, "EM.dat"))
+
+system.time(system("ss3_24o_safe"))
+# x <- r4ss::SS_output(dir = ".")
+# r4ss::SS_plots(x, png = TRUE)
+
+files <- list.files()
+files <- files[-which(files %in% original_files)]
+unlink(files)
