1bea5e91697c2eabb5d4b2ae8ba8bfb8c2cdb0d2
Sean Anderson <github@seananderson.ca>
2015-02-10 00:58:30 -0500

    Add option for optimized SS3 in check.R file

--- hake-em/check.R
+++ hake-em/check.R
@@ -4,7 +4,9 @@ original_files <- list.files()
 file.copy(paste0("../", species, "-om/", species, "OM.dat"),
   paste0(species, "EM.dat"))
 
-system.time(system("ss3_24o_safe"))
+system.time(system("ss3_24o_opt"))
+# system.time(system("ss3_24o_safe"))
+
 # x <- r4ss::SS_output(dir = ".")
 # r4ss::SS_plots(x, png = TRUE)
 
