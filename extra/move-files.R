a10f59a3032b042a8fd4563166012b7d3fcfa6a3
Sean Anderson <sean@seananderson.ca>
2015-02-13 18:05:16 -0500

    Rename .ctl and .dat files (and fix mackerel location)

---
+++ extra/move-files.R
@@ -0,0 +1,16 @@
+setwd("../inst")
+m <- list.files()
+ty <- c("om", "em")
+for (i in m) {
+  for (j in ty) {
+    setwd(paste0(i, "/", j))
+    f <- list.files()
+    ctl_file <- f[grepl("*\\.ctl", f)]
+    if (!file.exists("ss3.ctl")) { # already done?
+      if (j == "om") dat_file <- f[grepl("*\\.dat", f)]
+      system(paste("git mv", ctl_file, "ss3.ctl"))
+      if (j == "om") system(paste("git mv", dat_file, "ss3.dat"))
+    }
+    setwd("../../")
+  }
+}
