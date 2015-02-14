e359fdcbace5438eaf2472a11b38c5bd6728cb1b
Sean Anderson <sean@seananderson.ca>
2015-02-13 19:07:17 -0500

    Add check_models function

--- R/check_models.R
+++ R/check_models.R
@@ -1,13 +1,22 @@
 #' Check SS3 models
 #'
+#' @param Path to the model
+#' @param hess Should the hessian matrix be estimated?
+#'
 #' @export
 
-check_models <- function(model) {
+check_model <- function(model, hess = FALSE) {
   wd <- getwd()
   on.exit(setwd(wd))
   td <- tempdir()
-  file.copy(model, td)
+  f <- list.files(model)
+  tdf <- list.files(td)
+  unlink(tdf, recursive = TRUE, force = TRUE)
+  file.copy(file.path(model, f), td, overwrite = TRUE)
   setwd(td)
-  system("ss3_24o_safe")
+  if (hess)
+    system("ss3_24o_safe")
+  else
+    system("ss3_24o_safe -nohess")
   setwd(wd)
 }
