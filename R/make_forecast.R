c212a60086f9f87caa9d322e5b7396a04ada3d06
Sean Anderson <sean@seananderson.ca>
2015-02-16 21:10:15 -0500

    Strip whitespace

--- R/make_forecast.R
+++ R/make_forecast.R
@@ -5,15 +5,15 @@
 #'   The default is \code{"forecast.ss"}.
 #' @param dir A directory to save \code{outfile} in. The default is \code{NULL},
 #'   where the \code{outfile} will be saved in \code{getwd()}.
-#' @param msy Specifies whether or not to do a forecast and which \emph{F} to 
-#'   use for that forecast. Use \code{0} to have no forecast, \code{1} to set to 
+#' @param msy Specifies whether or not to do a forecast and which \emph{F} to
+#'   use for that forecast. Use \code{0} to have no forecast, \code{1} to set to
 #'   F(SPR), \code{2} to calculate F(MSY), \code{3} to set to F(Btarget),
 #'   and \code{4} to set to F(endyr). Default is \code{2}.
-#' @param spr_target Stock Synthesis searches for an \emph{F} multiplier that 
+#' @param spr_target Stock Synthesis searches for an \emph{F} multiplier that
 #'   will produce this level of spawning stock biomass (or reproductive output)
 #'   per recruit relative to an unfished value. Default is \code{0.4}.
-#' @param b_target Stock Synthesis searches for an \emph{F} multiplier that 
-#'   will produce this level of spawning stock biomass relative to an unfished 
+#' @param b_target Stock Synthesis searches for an \emph{F} multiplier that
+#'   will produce this level of spawning stock biomass relative to an unfished
 #'   value. This is not per recruit and takes into account the Spawner-Recruit
 #'   relationship. Default is \code{0.4}.
 #' @param bmark_years A vector of \code{length} 6: 1) beginning year of biology,
@@ -22,11 +22,11 @@
 #'   F. All entries are less than or equal to zero, to set years relative to the
 #'   terminal year of the model. Default is \code{c(0, 0, 0, 0, 0, 0)}.
 #' @param bmark_relf A numeric value of \code{1} or \code{2}, where the former
-#'   instructs Stock Synthesis to use the year range from \code{bmark_years} to 
+#'   instructs Stock Synthesis to use the year range from \code{bmark_years} to
 #'   calculate a relative fishing mortality level and the later uses the same
 #'   years as specified for the forecast in \code{fcast_years}.
-#' @param forecast Specifies whether or not to do a forecast benchmarks and which 
-#'   to calculate. Use \code{0} to have no forecast, \code{1} to set to 
+#' @param forecast Specifies whether or not to do a forecast benchmarks and which
+#'   to calculate. Use \code{0} to have no forecast, \code{1} to set to
 #'   F(SPR), \code{2} to calculate F(MSY), \code{3} to set to F(Btarget),
 #'   and \code{4} to set to F(endyr). Default is \code{0}. Inputs are the same
 #'   as \code{msy}.
@@ -36,9 +36,9 @@
 #'   relative to the end year of the simulation that will be used in the forecasts.
 #'   Values correspond to the beginning and end years for selectivity and the
 #'   relative \emph{F}s that will be used in population forecasts.
-#' @param fleet A numeric value assigning the forecast to a specific fleet in 
+#' @param fleet A numeric value assigning the forecast to a specific fleet in
 #'   the model. Default is fleet \code{1}.
-#' @param verbose A logical specifying whether or not to write information to the 
+#' @param verbose A logical specifying whether or not to write information to the
 #'   console while writing the forecast files. Useful for debugging.
 #'   Default is \code{FALSE}.
 #' @param write_file A logical specifying whether or not to write the file to the
@@ -48,17 +48,17 @@
 #' @importFrom r4ss SS_writeforecast
 #' @author Kelli Johnson
 
-make_forecast <- function(outfile = "forecast.ss", dir = NULL, msy = 2, 
-  spr_target = 0.4, b_target = 0.4, bmark_years = rep(0, 6), bmark_relf = 1, 
-  forecast = 0, nforecast = 0, forecast_years = rep(0, 4), fleet = 1, 
+make_forecast <- function(outfile = "forecast.ss", dir = NULL, msy = 2,
+  spr_target = 0.4, b_target = 0.4, bmark_years = rep(0, 6), bmark_relf = 1,
+  forecast = 0, nforecast = 0, forecast_years = rep(0, 4), fleet = 1,
   verbose = FALSE, write_file = TRUE) {
   # perform some checks
   if (length(bmark_years) != 6) {
-      stop(paste("bmark_years should be a vector of length six, not", 
+      stop(paste("bmark_years should be a vector of length six, not",
         length(bmark_years), "as entered."))
   }
   if (length(forecast_years) != 4) {
-      stop(paste("forecast_years should be a vector of length six, not", 
+      stop(paste("forecast_years should be a vector of length six, not",
         length(forecast_years), "as entered."))
   }
   if (length(fleet) > 1) {
