ce57307aeb828035b6612a9d1ace7c6db356d87f
Sean Anderson <sean@seananderson.ca>
2015-02-20 15:53:04 -0500

    Tweaks to check_model, see notes
    - Add binary name option
    - No need for return()
    - Use stringsAsFactors = FALSE
    - Add a couple examples

--- R/check_model.R
+++ R/check_model.R
@@ -4,12 +4,19 @@
 #' @param opts Options to pass to SS3 on the command line
 #' @param ss_mode One of \code{"safe"} or \code{"optimized"} for which SS3
 #'   binary to run. See the ss3sim vignette for instructions.
+#' @param ss_binary Name of the SS3 binary minus \code{_safe} or \code{_opt}.
+#'   This binary should be in your system's path.
 #'
 #' @export
 #' @return A data frame of warnings produced from SS3.
+#' @examples
+#' \dontrun{
+#' check_model(ss3model("hake", "om"), opts = "-noest")
+#' check_model(ss3model("hake", "em"), opts = "-nohess")
+#' }
 
 check_model <- function(model, opts = "-nohess",
-  ss_mode = c("safe", "optimized")) {
+  ss_mode = c("safe", "optimized"), ss_binary = "ss3_24o") {
 
   ss_mode <- match.arg(ss_mode)
   if(!ss_mode %in% c("safe", "optimized")) {
@@ -18,7 +25,7 @@ check_model <- function(model, opts = "-nohess",
     ss_mode <- "safe"
   }
   if(ss_mode == "optimized") ss_mode <- "opt"
-  ss_bin <- paste0("ss3_24o_", ss_mode)
+  ss_bin <- paste0(ss_binary, "_", ss_mode)
 
   wd <- getwd()
   on.exit(setwd(wd))
@@ -31,7 +38,8 @@ check_model <- function(model, opts = "-nohess",
   system(paste(ss_bin, opts))
   warns <- readLines("warning.sso")[-c(1:2)]
   warns <- warns[warns != ""]
-  warns <- data.frame("model" = model, "warnings" = warns)
+  warns <- data.frame("model" = model, "warnings" = warns,
+    stringsAsFactors = FALSE)
   setwd(wd)
-  return(warns)
+  warns
 }
